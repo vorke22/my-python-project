6d3e6e764434ffc3fbdf5d8f27445fb8943f781c
vorke <vokeakpos1@gmail.com>
2023-03-14 18:29:45 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+print(20*24*60)
+print ("20 days are "+str(50)+ "minutes")
+print ((f"20 days are {50} minutes" ))
+
+_name= ("vorke")
+print(_name)
+
+_age=38
+print(f"my name is {_name} and i am {_age}" )
+_sent=f"my name is {_name} and i am {_age}"
+print(_sent)
